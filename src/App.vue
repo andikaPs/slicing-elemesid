b65a0a689adb93631531a52274e390745ef83273
andikaPS <andikapermanasidiq00@gmail.com>
2023-02-02 14:12:19 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <div class="grid place-content-center h-screen">
+    <h1 class="text-2xl font-semibold text-gray-500 uppercase tracking-widest">Hello Vue</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
