3af49905ef6e0b664b1edb5f02740a2097316255
andikaPS <andikapermanasidiq00@gmail.com>
2023-02-02 15:14:29 +0700

    add rating component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
+  import Rating from './components/Rating.vue'
+
 </script>
 
 <template>
   <div class="grid place-content-center h-screen">
     <h1 class="text-2xl font-semibold text-gray-500 uppercase tracking-widest">Hello Vue</h1>
+    <Rating :rating="4.2" className="w-6 h-6" />
   </div>
 </template>
 
---
+++ src/components/Rating.vue
@@ -0,0 +1,30 @@
+<script setup>
+  import { ref } from 'vue'
+  import ic_star from '@/assets/icons/star.svg'
+  import ic_starplaceholder from '@/assets/icons/starplaceholder.svg'
+
+  const props = defineProps({
+    rating: Number,
+    className: String
+  })
+
+  const maxRating = ref(5)
+  const star = ref(props.rating <= maxRating.value ? Math.floor(props.rating) : maxRating.value)
+  const starPlaceholder = ref(Math.ceil(maxRating.value - star.value))
+
+</script>
+
+<template>
+  <div class="flex space-x-3">
+    <div v-for="item in star">
+      <img :src="ic_star" alt="icon star" :class="className" />
+    </div>
+    <div v-for="item in starPlaceholder">
+      <img :src="ic_starplaceholder" alt="icon star" :class="className" />
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
